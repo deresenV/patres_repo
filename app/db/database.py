fc4430ce5dc1ec8198bf0974c50e4f105c48596c
deresenV <deresenvil@gmail.com>
2025-05-18 15:12:24 +0300

    init db struct

---
+++ app/db/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from typing import AsyncGenerator
+
+from app.config import config
+
+
+# Создание асинхронного движка
+engine = create_async_engine(config.SQLALCHEMY_DATABASE_URL, echo = True)
+
+# Создание фабрики асинхронных сессий
+AsyncSessionLocal = sessionmaker(
+    bind = engine,
+    class_= AsyncSession,
+    expire_on_commit = False
+)
+
+# Базовый класс для моделей
+Base = declarative_base()
+
+# Генератор сессий
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
